7d3d1819d30598c853c8ebf84d5b203f53e37e61
srichins <shaun@sdrichins.com>
2013-04-02 21:04:38 -0600

    Initial Commit

---
+++ compiler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  compiler
+//
+//  Created by srichins on 4/2/13.
+//  Copyright (c) 2013 srichins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
